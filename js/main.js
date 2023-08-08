5bce1af10a2fefec076b6b658824df03b8596c10
Mmoskwi4 <120642602+Mmoskwi4@users.noreply.github.com>
2023-08-08 15:40:32 +0300

    Add files via upload
    Merge adaptive index.html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 const swiper = new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
     },
+=======
+const swiper = new Swiper(".swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+>>>>>>> 43aa58d (Add Slider)
 });
